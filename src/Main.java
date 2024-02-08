d0bd7b6d92f37295f1b282dee0744d4e79b9af95
Knellec <ethancknell@gmail.com>
2024-02-07 22:17:32 -0500

    Finished program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Declare variables
+        double INTEREST_RATE = 0.17;
+        double cardBalance = 5000;
+        double monthOneInterest = 0;
+        double monthTwoInterest = 0;
+
+        // Calculate interest
+        monthOneInterest = cardBalance * INTEREST_RATE;
+        cardBalance = cardBalance + monthOneInterest;
+        monthTwoInterest = monthOneInterest + cardBalance * INTEREST_RATE;
+
+        // Display
+        System.out.println("The interest after 1 month is $" + monthOneInterest);
+        System.out.println("The interest after 2 months is $" + monthTwoInterest);
+
+
+
     }
 }
